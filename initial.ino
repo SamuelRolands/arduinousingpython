abdb0a500683444ce0365f106024a18bab1a354e
SamuelRolands <samuelrolands27@gmail.com>
2024-10-15 00:00:15 +0530

    Red Green LED Test

---
+++ initial.ino
@@ -0,0 +1,33 @@
+const int redPin = 2;
+const int greenPin = 4;
+void setup() {
+  Serial.begin(9600);
+  pinMode(greenPin, OUTPUT);
+  pinMode(redPin, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available() > 0)
+  {
+    String msg =Serial.readString();
+
+    if (msg == "ON")
+    {
+      digitalWrite(greenPin, HIGH);
+    }
+    else if (msg ==  "OFF")
+    {
+      (greenPin, LOW);
+    }
+    else
+    {
+      for (int i = 0; i < 4; i++)
+      {
+        digitalWrite(redPin,HIGH);
+        delay(100);
+        digitalWrite(redPin, LOW);
+        delay(100);
+      }
+    }
+  }
+}
